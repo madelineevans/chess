acdfe94e67ee90ed703a329a03c76e4a6e90fc3d
Madeline Evans <maemaebooks@gmail.com>
2025-01-18 11:51:09 -0700

    Added all the pieces implementation framework

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -15,8 +15,42 @@ class BishopMovesCalculator implements PieceMovesCalculator{
     }
 }
 
-//class KingMovesCalculator implements PieceMovesCalculator{}
-//class PawnMovesCalculator implements PieceMovesCalculator{}
-//class KnightMovesCalculator implements PieceMovesCalculator{}
-//class QueenMovesCalculator implements PieceMovesCalculator{}
-//class RookMovesCalculator implements PieceMovesCalculator{}
+class KingMovesCalculator implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
+        System.out.println("All possible king moves:");
+        return new ArrayList<>();
+    }
+}
+
+class PawnMovesCalculator implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
+        System.out.println("All possible pawn moves:");
+        return new ArrayList<>();
+    }
+}
+
+class RookMovesCalculator implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
+        System.out.println("All possible rook moves:");
+        return new ArrayList<>();
+    }
+}
+
+class KnightMovesCalculator implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
+        System.out.println("All possible knight moves:");
+        return new ArrayList<>();
+    }
+}
+
+class QueenMovesCalculator implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
+        System.out.println("All possible queen moves:");
+        return new ArrayList<>();
+    }
+}
