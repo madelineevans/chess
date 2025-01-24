5d6deefb8b12f55f1baa6f03d9607b8055113230
Madeline Evans <maemaebooks@gmail.com>
2025-01-23 21:51:55 -0700

    working on pawn

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -6,21 +6,36 @@ import java.util.List;
 interface PieceMovesCalculator {
     Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition);
 
-    default Collection<ChessMove> canMove(ChessBoard board, ChessPosition myPosition, ArrayList<ChessMove> allMoves, int[][] directions, int row, int col) {
+//    default ChessMove move(ChessBoard board, ChessPosition myPosition, ChessPosition pos, int row, int col) {
+//        ChessMove move = null;
+//        if (pos.getRow() >= 1 && pos.getRow() <= 8 && pos.getColumn() >= 1 && pos.getColumn() <= 8) {
+//            if (board.getPiece(pos) == null) {
+//                System.out.println(row + ", " + col);
+//                return move = new ChessMove(myPosition, pos, null);
+//            }
+//            else if (board.getPiece(pos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+//                System.out.println(row + ", " + col);
+//                return move = new ChessMove(myPosition, pos, null);
+//                //remove the enemy piece right here???
+//            }
+//        }
+//        return move;
+//    }
+
+    default Collection<ChessMove> canMove(ChessBoard board, ChessPosition myPosition, int[][] directions) {
+        ArrayList<ChessMove> allMoves = new ArrayList<>();
         for (int[] direction : directions) {
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
             ChessPosition pos = new ChessPosition(row+direction[0], col+direction[1]);
-            System.out.println(pos.toString());
             if (pos.getRow() >= 1 && pos.getRow() <= 8 && pos.getColumn() >= 1 && pos.getColumn() <= 8) {
                 if (board.getPiece(pos) == null) {
-                    System.out.println(row + ", " + col);
                     ChessMove move = new ChessMove(myPosition, pos, null);
                     allMoves.add(move);
                 }
                 else if (board.getPiece(pos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                    System.out.println(row + ", " + col);
                     ChessMove move = new ChessMove(myPosition, pos, null);
                     allMoves.add(move);
-                    //remove the enemy piece right here???
                 }
             }
         }
@@ -43,7 +58,8 @@ interface PieceMovesCalculator {
                     if (board.getPiece(pos) == null) {
                         ChessMove move = new ChessMove(myPosition, pos, null);
                         allMoves.add(move);
-                    } else if (board.getPiece(pos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    }
+                    else if (board.getPiece(pos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
                         run = false;
                         ChessMove move = new ChessMove(myPosition, pos, null);
                         allMoves.add(move);
@@ -77,40 +93,65 @@ class RookMovesCalculator implements PieceMovesCalculator{
 class KingMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
-        ArrayList<ChessMove> allMoves = new ArrayList<>();
-
         int[][] directions = {{1,0}, {1,1}, {0,1}, {1,-1}, {0,-1}, {-1,1}, {-1,0}, {-1,-1}};
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
-        System.out.println("King pos: " + row + ", " + col);
-
-        return canMove(board, myPosition, allMoves, directions, row, col);
+        return canMove(board, myPosition, directions);
     }
 }
 
 class PawnMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
-        ArrayList<ChessMove> allMoves = new ArrayList<>();
-        ArrayList<int[]> allDir = new ArrayList<>();
+        ArrayList<ChessMove> allMoves = new ArrayList<>(); //moves to return
+        ArrayList<Integer> allDir = new ArrayList<>();      //directions to move piece
+        ArrayList<int[]> dir = new ArrayList<>();       //directions to check if take another piece
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
+        boolean found = false;
+
+        System.out.println("pawn at "+row+","+col);
 
         if (board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.BLACK) {       //if black
-            //if
-            allDir.add(new int[]{-1,0});
+            allDir.add(-1);
             if(row == 7){
-                allDir.add(new int[]{-2,0});
+                allDir.add(-2);
             }
         }
         else{       //if white
-            allDir.add(new int[]{1,0});
+            allDir.add(1);
             if(row== 2){
-                allDir.add(new int[]{2,0});
+                allDir.add(2);
             }
         }
 
+        System.out.println("pawn can move: ");
+        for(Integer di : allDir){
+            System.out.println("dir "+di);
+            if(di<0){//if black
+                dir.add(new int[]{-1,-1});
+                dir.add(new int[]{-1,1});
+            }
+            else{//if white
+                dir.add(new int[]{1,-1});
+                dir.add(new int[]{1,1});
+            }
 
+
+            ChessPosition pos = new ChessPosition(row+di, col);
+
+            if (pos.getRow() >= 1 && pos.getRow() <= 8 && pos.getColumn() >= 1 && pos.getColumn() <= 8) {
+                for(int[] d: dir){
+                    if (board.getPiece(pos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) { //can I take a piece
+                        ChessMove move = new ChessMove(myPosition, pos, null);
+                        allMoves.add(move);
+                        found = true;
+                    }
+                }
+                if (!found && board.getPiece(pos) == null) { //else if no one's blocking it's move
+                    ChessMove move = new ChessMove(myPosition, pos, null);
+                    allMoves.add(move);
+                }
+            }
+        }
         return allMoves;
     }
 }
@@ -119,11 +160,7 @@ class KnightMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> findAllMoves(ChessBoard board, ChessPosition myPosition){
         int[][] directions = {{2,1},{-2,1},{-2,-1},{2,-1},{1,2},{-1,2},{-1,-2},{1,-2}};
-        ArrayList<ChessMove> allMoves = new ArrayList<>();
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
-
-        return canMove(board, myPosition, allMoves, directions, row, col);
+        return canMove(board, myPosition, directions);
     }
 }
 
