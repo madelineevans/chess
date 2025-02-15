a0d802184c996528dc4aeb8e94028a85d32f5e55
Madeline Evans <maemaebooks@gmail.com>
2025-02-15 16:09:15 -0700

    Phase 3 setup stuff

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Server: " + piece);
-        Server s = new Server();
-        s.run(8080);
+        try{
+            var port = 8080;
+            Server s = new Server();
+            s.run(port);
+            System.out.printf("Server started on port %d", port);
+            return;
+        } catch(Throwable ex){
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
 }
