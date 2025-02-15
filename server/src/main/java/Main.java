8cce121795bd47d397e5eb6d46e729f8139c18a6
Madeline Evans <maemaebooks@gmail.com>
2025-02-15 15:55:51 -0700

    Phase 3 setup stuff

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Server: " + piece);
         Server s = new Server();
-        s.run(8008);
+        s.run(8080);
     }
 }
