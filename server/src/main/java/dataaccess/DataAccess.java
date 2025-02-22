2acfd8227e171518277d6516404ef2ec6f334018
Madeline Evans <maemaebooks@gmail.com>
2025-02-22 09:56:00 -0700

    adding UserDAO implementations/functions

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,10 +1,9 @@
 package dataaccess;
 import model.Data;
-import model.UserData;
 
 public interface DataAccess {
     Data createData();
     Data readData();
     Data updateData();
-    Data deleteData();
+    void deleteData();
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,13 +1,30 @@
 package dataaccess;
 import model.UserData;
+import java.util.Map;
+import java.util.HashMap;
 
 public class UserDAO implements DataAccess {
+    final private Map<String, UserData> users = new HashMap<>();
 
     @Override
-    public UserData createData(String u, String p, String e) {
+    public UserData createData(String u, String p, String e) throws DataAccessException{
         UserData user = new UserData(u, p, e);
-
-        //users.put
+        users.put(u, user);
         return user;
+    }
+
+    @Override
+    public UserData readData(String u){
+        return users.get(u);
+    }
+
+//    @Override
+//    public UserData updateData(){
+//
+//    }
+
+    @Override
+    public void deleteData(){
+        users.clear();
     }
 }
