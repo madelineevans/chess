3baf3b77750f7fa1916dae2cece28df00020be2b
Madeline Evans <maemaebooks@gmail.com>
2025-03-12 17:41:15 -0600

    getting rid of duplicate methods

--- server/src/main/java/dataaccess/SqlGameDAO.java
+++ server/src/main/java/dataaccess/SqlGameDAO.java
@@ -117,51 +117,6 @@ public class SqlGameDAO implements GameDAO {
         return game.setGameID(gameID);
     }
 
-//    private String updateGameJson(GameData game){
-//        var json = new Gson().toJson(game);
-//        return json;
-//    }
-
-    private int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()){
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)){
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case null -> ps.setNull(i + 1, NULL);
-                        default -> {
-                        }
-                    }
-                }
-                ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        }
-    }
-
-    private void configureDatabase(String[] createStatements) throws DataAccessException{
-        DatabaseManager.createDatabase();
-        try(var conn = DatabaseManager.getConnection()){
-            for(var statement : createStatements){
-                try(var preparedStatement = conn.prepareStatement(statement)){
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException ex){
-            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
-        }
-    }
-
     private final String[] createStatements = {
             """            
             CREATE TABLE IF NOT EXISTS  game (
--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -9,9 +9,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.sql.*;
 
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
-import static java.sql.Types.NULL;
-
 public class SqlUserDAO implements UserDAO {
 
     public SqlUserDAO() throws DataAccessException{
@@ -34,6 +31,7 @@ public class SqlUserDAO implements UserDAO {
             throw new DataAccessException("Error checking username preexistance" + e.getMessage());
         }
         var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        System.out.println("Original Password: " + user.password());
         var id = executeUpdate(statement, user.username(), createHashedPassword(user.password()), user.email());
     }
 
@@ -110,7 +108,10 @@ public class SqlUserDAO implements UserDAO {
     }
 
     private String createHashedPassword(String clearTextPassword) {
-        return BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
+        String hashed = BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
+        System.out.println("Hashed Password: " + hashed);
+        return hashed;
+        //return BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
     }
 
     @Override
@@ -139,46 +140,6 @@ public class SqlUserDAO implements UserDAO {
             }
         } catch (Exception e){
             throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
-        }
-    }
-
-    private int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()){
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)){
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case null -> ps.setNull(i + 1, NULL);
-                        default -> {
-                        }
-                    }
-                }
-                ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        }
-    }
-
-    private void configureDatabase(String[] createStatements) throws DataAccessException{
-        DatabaseManager.createDatabase();
-        try(var conn = DatabaseManager.getConnection()){
-            for(var statement : createStatements){
-                try(var preparedStatement = conn.prepareStatement(statement)){
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException ex){
-            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
         }
     }
 
